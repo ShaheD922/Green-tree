ad0bc1cbdc95609786dd18f239485aa4ff55f02b
ShaheD922 <shahedhasan.yc@gmail.com>
2025-09-08 18:19:47 +0600

    all container i caught with container id

---
+++ index.js
@@ -0,0 +1,10 @@
+const categoryContainer =document.getElementById("categoryContainer");
+const treeContainer =document.getElementById("treeContainer");
+const cartContainer = document.getElementById("cartContainer");
+const cartTotal =document.getElementById("cartTotal");
+const modalContainer = document.getElementById("modalContainer");
+const treeModal = document.getElementById("tree-details-modal");
+
+let cart = [];
+let allPlants = [];
+let allCategories = [];
